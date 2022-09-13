b07346bfa67e2bdf86a12c18c93895ea294fd5c5
abimbade babawale <112903730+abimbaba@users.noreply.github.com>
2022-09-13 08:08:36 +0100

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,11 @@
-function secondLargest(arr) {
-  
+unction countTruthy(arr) {
+    let result = []
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] > 0) {
+            result.push(arr[i])
+        }
+    } return result.length
 }
-module.exports = secondLargest
+console.log(countTruthy([6, 3, 0, 30, 7]))   //  4
+console.log(countTruthy(["", 3, 0, 30, 7]))  // 3
+console.log(countTruthy(["", 0, 0, undefined, 7]))  //1
