621c3b4e7c960890dbbe8cb3699019d56aac9295
abimbade babawale <112903730+abimbaba@users.noreply.github.com>
2022-09-13 08:10:25 +0100

    Update challenge-2.js

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,5 +1,15 @@
-function filterArray(arr) {
- 
+function FizzBuzz(input) {
+    if (input % 3 ==0 && input % 5 == 0) {
+        return "fizzbuzz"
+    } else if (input % 3 == 0){
+        return "fizz"
+    } else if (input % 5 == 0){
+        return "buzz"
+    } else {
+        return `Mrs Faith did not specify what to do....lol, because ${input} is not divisible by both 3 and 5 `
+    }
 }
-
-module.exports = filterArray
+console.log(FizzBuzz(3))
+console.log(FizzBuzz(5))
+console.log(FizzBuzz(15))
+console.log(FizzBuzz(8))
