8feec8f7c82386083077337a7df3e54b849c7ad0
abimbade babawale <112903730+abimbaba@users.noreply.github.com>
2022-09-13 12:15:41 +0100

    Update challenge-5.js

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,13 @@
-function filterList(arr) {
- 
- }
+unction filterList (arr) {
+    let integer = []
+    for (let i = 0; i < arr.length; i++){
+        if (typeof arr[i] == "number"){
+            integer.push(arr[i])
+        } 
+       
+    }return integer
+}
 
-  module.exports =filterList
+console.log(filterList([1,2,'a','b'])) //to return [1,2]
+console.log(filterList([1,'a','b',0,15])) //to return [1,0,15]
+console.log(filterList([1,2,'aasf','1','123',123])) //to return [1,2,123]
