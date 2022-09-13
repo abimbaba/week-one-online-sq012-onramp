b075bd1ad16fa0db21ca4f11ba090ca157fd0984
abimbade babawale <112903730+abimbaba@users.noreply.github.com>
2022-09-13 12:10:49 +0100

    Update challenge-3.js

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,5 +1,11 @@
 function sumMix(arr) {
-   
- }
+    //let result = []
+    let total = 0
+    for (let i = 0; i < arr.length; i++){
+        total = total + (+arr[i])
+    }return total
+}
 
-  module.exports = sumMix
+console.log(sumMix([9, 3, '7', '3']))
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']))
